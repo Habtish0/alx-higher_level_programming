bdb6698bce09f0bc3e2bd783267ef1aa15681cbd
Habtish0 <habtu.ke@gmail.com>
2023-06-12 20:56:41 +0000

    task 02

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
 const count = process,argv.length;
-console.log(count === 2 ? 'no argument' : count ===3 ? 'Argument found' : 'Arguments found');
- 
+console.log(count === 2 ? 'no argument' : count ===3 ? 'Argument found' : 'Arguments found'); 
